048f5b834c8b33c34e572bc60c55b19ad72a5126
Micah Lundquist <mlundquist001@student.franciscan.edu>
2024-09-04 10:55:50 -0400

    added 'r' to refresh

--- app.js
+++ app.js
@@ -59,23 +59,25 @@ window.gridW = 100; // size of plane
 window.gridH = 100; // size of plane
 window.noiseCurve = 2; // makes higher highs and lower lows 2.3
 
-let waterLevel = noiseLevel*0.13// sets the hieght of the water plane.
+let waterPlaneLevel = noiseLevel*0.13// sets the hieght of the water plane.
 // waterLevel COULD be calculated if I make the UV interp functions with variables
 window.debugClipping = false
 
 let moving = false; // makes the world infinitely generate!
 let timeScale = 0.2; // Speed at which world moves, 0.2
 
-heightMap = new Array(lenx*leny)
-moistMap = new Array(lenx*leny)
-fillHeightWithNoise()
-fillMoistureWithNoise()
 
 camera.position.z = 60
 camera.position.x = 60
 camera.position.y = 35
 
-// CREATE RECTS
+// CREATE MAP GEOMETRY
+
+heightMap = new Array(lenx*leny)
+moistMap = new Array(lenx*leny)
+fillHeightWithNoise()
+fillMoistureWithNoise()
+
 const pw = gridW
 const ph = gridH
 const wsegs = lenx-1 // I FUCKING FIXED IT I LOOKED FOR LIKE A WHOLE DAY
@@ -116,7 +118,7 @@ const planeMat = new THREE.MeshLambertMaterial({
     vertexColors: true,
     side: THREE.DoubleSide // renders both sides, could avoid for more performance
 })
-const wholeMap = new THREE.Mesh(planeGeo, planeMat)
+ window.wholeMap = new THREE.Mesh(planeGeo, planeMat)
 
 wholeMap.rotation.x = -Math.PI /2
 
@@ -142,7 +144,7 @@ const waterMat = new THREE.MeshLambertMaterial({
 // })
 const water = new THREE.Mesh(waterGeo, waterMat);
 water.rotation.x = -Math.PI/2
-water.position.y = waterLevel
+water.position.y = waterPlaneLevel
 scene.add(water)
 
 
@@ -154,6 +156,7 @@ scene.add(water)
 
 // create canvas 1
 const canv1 = document.createElement('canvas');
+canv1.id = "height"
 canv1.width = 180
 canv1.height = 180
 document.body.appendChild(canv1)
@@ -161,6 +164,7 @@ const ctx1 = canv1.getContext('2d');
 
 // create canvas 2
 const canv2 = document.createElement('canvas');
+canv2.id = "moist"
 canv2.width = 180
 canv2.height = 180
 document.body.appendChild(canv2)
@@ -312,6 +316,120 @@ function getPixel(x,y){
     return [UVmap[idxR],UVmap[idxG],UVmap[idxB]]
 }
 
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event){
+    var keyCode = event.which;
+    if (keyCode == 82){
+        refresh()
+    }
+}
 
+function refresh(){
+scene.remove(wholeMap)
+heightMap = new Array(lenx*leny)
+moistMap = new Array(lenx*leny)
+fillHeightWithNoise()
+fillMoistureWithNoise()
+
+const pw = gridW
+const ph = gridH
+const wsegs = lenx-1 // I FUCKING FIXED IT I LOOKED FOR LIKE A WHOLE DAY
+const hsegs = leny-1 // AND IT WAS CUZ I DIDNT PUT -1 👹👹👹
+const planeGeo = new THREE.PlaneGeometry(pw, ph, wsegs, hsegs);
+
+// displace the vertices based on height data
+const colors = []
+
+
+planeGeo.attributes.position.count == lenx*leny // FALSE
+for (let i = 0; i < planeGeo.attributes.position.count; i++) {
+   
+    const vertex = new THREE.Vector3()
+    vertex.fromBufferAttribute(planeGeo.attributes.position, i);
+    
+    vertex.z = noiseLevel * heightMap[i]
+
+    planeGeo.attributes.position.setXYZ(i, vertex.x, vertex.y, vertex.z);
+   
+    //assign vertex color
+    let UVheight = Math.floor(interp(heightMap[i], 0, 1, 0, 100))
+    let UVmoist = Math.floor(interp(moistMap[i], 0, 1, 0, 100))
+    let UVPixel = getPixel(100-UVmoist, 100-UVheight)
+    let r = interp(UVPixel[0], 0,255,0,1)
+    let g = interp(UVPixel[1], 0,255,0,1)
+    let b = interp(UVPixel[2], 0,255,0,1)
+    const color = new THREE.Color(r,g,b);
+    colors.push(color.r, color.g, color.b)
+}
+
+planeGeo.setAttribute('color', new THREE.Float32BufferAttribute(colors,3))
+
+planeGeo.attributes.position.needsUpdate = true;
+planeGeo.computeVertexNormals()
+
+const planeMat = new THREE.MeshLambertMaterial({
+    vertexColors: true,
+    side: THREE.DoubleSide // renders both sides, could avoid for more performance
+})
+
+
+wholeMap = new THREE.Mesh(planeGeo, planeMat)
+
+wholeMap.rotation.x = -Math.PI /2
+
+scene.add(wholeMap)
+
+
+
+// DO NOISE CANVAS REFRESH
+
+// set Image Data on canvas 1 to heightmap
+const canv1 = document.getElementById("height");
+const canv2 = document.getElementById("moist");
+let ctx1 = canv1.getContext('2d')
+let ctx2 = canv2.getContext('2d')
+
+
+const imgData1 = ctx1.createImageData(canv1.width,canv1.height);
+
+for (let y=0; y < canv1.height; y++){
+    for (let x=0; x < canv1.width; x++){
+        let tx = Math.floor(interp(x,0,canv1.width,0,lenx)) // scale down the image
+        let ty = Math.floor(interp(y,0,canv1.height,0,leny))
+        const index = (tx+ty*lenx) 
+        const value = heightMap[index]
+        const pixelIndex = (x+y*canv1.height)*4
+        const color = new THREE.Color().setHSL(0.325, 0.5, value)
+
+        imgData1.data[pixelIndex] = color.r*255
+        imgData1.data[pixelIndex+1] = color.g*255
+        imgData1.data[pixelIndex+2] = color.b*255
+        imgData1.data[pixelIndex+3] = 255
+    }
+}
+ctx1.putImageData(imgData1, 0, 0)
+
+// set Image Data on canvas 2 to moistmap
+const imgData2 = ctx2.createImageData(canv2.width,canv2.height);
+
+for (let y=0; y < canv2.height; y++){
+    for (let x=0; x < canv2.width; x++){
+        let tx = Math.floor(interp(x,0,canv2.width,0,lenx)) // scale down the image
+        let ty = Math.floor(interp(y,0,canv2.height,0,leny))
+        const index = (tx+ty*lenx) 
+        const value = moistMap[index]
+        const pixelIndex = (x+y*canv2.height)*4
+        const color = new THREE.Color().setHSL(0.616, 0.5, value)
+
+        imgData2.data[pixelIndex] = color.r*255
+        imgData2.data[pixelIndex+1] = color.g*255
+        imgData2.data[pixelIndex+2] = color.b*255
+        imgData2.data[pixelIndex+3] = 255
+    }
+}
+ctx2.putImageData(imgData2, 0, 0)
+
+
+}
 
 loadImage() // runs setup upon completion.
